79a64e6045eee9d37f625dfb8b37a84675574bfa
Red <theredstar@aliyun.com>
2021-08-13 11:38:15 +0800

    feat: create warehouse scanner page.

--- website/pages/warehouse/scanner/index.jsx
+++ website/pages/warehouse/scanner/index.jsx
@@ -8,21 +8,38 @@ export default defineComponent({
     const menus = ref([
       {
         label: "出仓扫描",
+        icon: "export",
+        badgeColor: "warning",
       },
       {
         label: "归仓扫描",
+        icon: "import",
+        badgeColor: "success",
       },
       {
         label: "紧急扫描",
+        icon: "emergency",
+        badgeColor: "danger",
       },
     ]);
 
     return () => (
       <section className="h-full flex flex-row items-center justify-center">
-        <div className="grid grid-cols-3 gap-x-62">
+        <div className="w-3/5 h-2/5 grid grid-cols-3 gap-x-62">
           {menus.value.map((menuItem) => {
             return (
-              <div className="dark:bg-navy-2 rounded">{menuItem.label}</div>
+              <div className="flex flex-col items-center justify-center bg-navy-2 hover:bg-navy-1 rounded text-16 text-center font-medium">
+                <img
+                  src={`assets/icon_scan_${menuItem.icon}.png`}
+                  alt={menuItem.label}
+                />
+                <p className="flex flex-row items-center py-8 text-white text-opacity-90">
+                  <i
+                    className={`inline-block w-16 h-16 mr-8 rounded-full bg-${menuItem.badgeColor}`}
+                  />
+                  {menuItem.label}
+                </p>
+              </div>
             );
           })}
         </div>
