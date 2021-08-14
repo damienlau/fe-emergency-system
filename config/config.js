5ae288fc558f3218d8b86e7abde655e5663cda5d
Red <theredstar@aliyun.com>
2021-08-14 10:50:19 +0800

    feat: create iconfont component.

---
+++ config/config.js
@@ -0,0 +1,5 @@
+const configs = {
+  iconUrl: "//at.alicdn.com/t/font_2563603_nynecugp4bp.js",
+};
+
+export default configs;
---
+++ website/components/Icon/index.jsx
@@ -0,0 +1,20 @@
+import { defineComponent } from "vue";
+import { createFromIconfontCN } from "@ant-design/icons-vue";
+import defaultConfig from "config/config";
+
+export default defineComponent({
+  props: {
+    type: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    const { iconUrl } = defaultConfig;
+    const Icon = createFromIconfontCN({
+      scriptUrl: iconUrl,
+    });
+
+    return () => <Icon type={`rs-icon-${props.type}`} />;
+  },
+});
