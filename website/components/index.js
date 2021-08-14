2352915ce263ff78929e956fde1b7cbd028dc9af
Red <theredstar@aliyun.com>
2021-08-14 10:54:38 +0800

    feat: new components.
    add Form & Icon component.

---
+++ website/components/index.js
@@ -0,0 +1,5 @@
+import Form from "./Form";
+import Icon from "./Icon";
+import Modal from "./Modal";
+
+export { Form, Icon, Modal };
--- website/main.js
+++ website/main.js
@@ -1,9 +1,11 @@
 import { createApp } from "vue";
 import App from "./App";
+import Antd from "ant-design-vue";
 import Router from "./router";
 import "../less/index.less";
 
 const app = createApp(App);
 
+app.use(Antd);
 app.use(Router);
 app.mount("#redstar");
