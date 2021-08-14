5f85f3f149fd988ec0e8662f9668b5b3f4ad6dab
Red <theredstar@aliyun.com>
2021-08-14 10:49:42 +0800

    fix: add modal different size class.
    fixed some style.

---
+++ website/components/Modal/index.jsx
@@ -0,0 +1,64 @@
+// 组件-模态框
+
+import { defineComponent, toRefs } from "vue";
+import { Icon } from "website/components";
+
+export default defineComponent({
+  name: "Modal",
+  props: {
+    visible: {
+      type: Boolean,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: false,
+    },
+    size: {
+      type: String,
+      required: false,
+      default: "light",
+      validator(size) {
+        return ["ultralight", "lighter", "light", "heavy", "bold"].includes(
+          size
+        );
+      },
+    },
+  },
+  emits: ["update:visible"],
+  setup(props, { slots, emit }) {
+    const { visible } = toRefs(props);
+
+    // 监听模态框关闭事件回调
+    const handleCloseModal = () => emit("update:visible", !visible.value);
+
+    return () => (
+      <a-modal
+        visible={visible.value}
+        class={`h-modal-${props.size} w-modal-${props.size} bg-modal-${props.size}`}
+        class="bg-no-repeat pb-0"
+        centered
+        closable={false}
+        footer={null}
+        onCancel={handleCloseModal}
+      >
+        <div class={`h-modal-${props.size} w-modal-${props.size}`} class="p-32">
+          {/* 模态框头部 */}
+          <div className="pb-24 flex flex-row items-center justify-center">
+            <h3 className="flex-auto text-20 text-center font-medium">
+              {props.title}
+            </h3>
+            <button
+              className="bg-navy-1 hover:bg-navy-2 w-24 h-24 flex items-center justify-center rounded"
+              onClick={handleCloseModal}
+            >
+              <Icon type="close" />
+            </button>
+          </div>
+          {/* 模态框主体 */}
+          <div>{slots.default && slots.default()}</div>
+        </div>
+      </a-modal>
+    );
+  },
+});
--- website/pages/warehouse/scanner/index.jsx
+++ website/pages/warehouse/scanner/index.jsx
@@ -1,6 +1,7 @@
 // 出/归仓扫描
 
 import { defineComponent, ref } from "vue";
+import { Modal } from "website/components";
 
 export default defineComponent({
   setup() {
@@ -22,28 +23,45 @@ export default defineComponent({
         badgeColor: "danger",
       },
     ]);
+    // 模态框是否可见
+    const visible = ref(false);
 
     return () => (
-      <section className="h-full flex flex-row items-center justify-center">
-        <div className="w-3/5 h-2/5 grid grid-cols-3 gap-x-62">
-          {menus.value.map((menuItem) => {
-            return (
-              <div className="flex flex-col items-center justify-center bg-navy-2 hover:bg-navy-1 rounded text-16 text-center font-medium">
-                <img
-                  src={`assets/icon_scan_${menuItem.icon}.png`}
-                  alt={menuItem.label}
-                />
-                <p className="flex flex-row items-center py-8 text-white text-opacity-90">
-                  <i
-                    className={`inline-block w-16 h-16 mr-8 rounded-full bg-${menuItem.badgeColor}`}
+      <>
+        {/* 扫描菜单-容器 */}
+        <section className="h-full flex flex-row items-center justify-center">
+          <div className="w-3/5 h-2/5 grid grid-cols-3 gap-x-62">
+            {menus.value.map((menuItem) => {
+              return (
+                // 扫描菜单-卡片
+                <div
+                  className="flex flex-col items-center justify-center bg-navy-2 hover:bg-navy-1 hover:cursor-pointer rounded text-16 text-center font-medium"
+                  onClick={() => (visible.value = !visible.value)}
+                >
+                  {/* 扫描菜单-卡片图标 */}
+                  <img
+                    src={`assets/icon_scan_${menuItem.icon}.png`}
+                    alt={menuItem.label}
                   />
-                  {menuItem.label}
-                </p>
-              </div>
-            );
-          })}
-        </div>
-      </section>
+                  {/* 扫描菜单-卡片标题 */}
+                  <p className="flex flex-row items-center py-8 text-white text-opacity-90">
+                    <i
+                      className={`inline-block w-16 h-16 mr-8 rounded-full bg-${menuItem.badgeColor}`}
+                    />
+                    {menuItem.label}
+                  </p>
+                </div>
+              );
+            })}
+          </div>
+        </section>
+        {/* 扫描菜单-模态框 */}
+        <Modal
+          v-model={[visible.value, "visible"]}
+          size="ultralight"
+          title="请填写信息"
+        ></Modal>
+      </>
     );
   },
 });
