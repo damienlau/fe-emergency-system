5b9f5815cf6fb4d31bb96dc66cbad988b73f1eff
Red <theredstar@aliyun.com>
2021-08-17 19:07:41 +0800

    fix: change iconfont url.

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const configs = {
   title: "深圳多特医疗技术有限公司",
-  iconUrl: "//at.alicdn.com/t/font_2563603_zee92a8rpb.js",
+  iconUrl: "//at.alicdn.com/t/font_2563603_ley2rpx24zd.js",
 };
 
 export default configs;
