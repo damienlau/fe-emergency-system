1114477b586a023f30112e497d027c93270d84e3
Red <theredstar@aliyun.com>
2021-08-17 19:08:06 +0800

    feat: create card component.

--- website/components/Card/index.jsx
+++ website/components/Card/index.jsx
@@ -4,12 +4,26 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "Card",
-  props: {},
+  props: {
+    title: { type: String, required: false },
+  },
   setup(props, { slots, emit }) {
-    <div class="bg-navy-2 hover:bg-navy-1">
-      {/* 卡片头部 */}
-      <div class="px-16"></div>
-      {/* 卡片主体 */}
-    </div>;
+    return () => (
+      <div class="flex flex-col bg-navy-2 hover:bg-navy-1 rounded">
+        {/* 卡片头部 */}
+        <div class="flex flex-row items-center justify-between px-16 py-4 border-b border-navy-3">
+          {/* 头部标题 */}
+          {slots.title ? (
+            slots.title()
+          ) : (
+            <span class="text-16 font-medium">{props.title}</span>
+          )}
+          {/* 头部操作区 */}
+          {slots.extra && slots.extra()}
+        </div>
+        {/* 卡片主体 */}
+        <div class="flex-1 p-16">{slots.default && slots.default()}</div>
+      </div>
+    );
   },
 });
