1dfbacd3835deb002f7e1bb1bfa55bef7febe39e
libo <1186279432@qq.com>
2021-08-27 20:11:41 +0800

    feat: 新弹框组件

--- website/components/index.js
+++ website/components/index.js
@@ -4,8 +4,20 @@ import Form from "./Form";
 import Icon from "./Icon";
 import Menu from "./Menu";
 import Modal from "./Modal";
+import Modalpending from "./Modalpending";
 import PageHeader from "./PageHeader";
 import Tabs from "./Tabs";
 import TableSelct from "./TableSelct";
 
-export { Card, Empty, Form, Icon, Menu, Modal, PageHeader, Tabs, TableSelct };
+export {
+  Card,
+  Empty,
+  Form,
+  Icon,
+  Menu,
+  Modal,
+  Modalpending,
+  PageHeader,
+  Tabs,
+  TableSelct,
+};
