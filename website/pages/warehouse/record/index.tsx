ca5ab128f624c8d49e2ac3268db2a1c3e704cbad
zhangjinjian <1016417093@qq.com>
2021-09-02 09:41:24 +0800

    pref: 优化借还记录代码

--- website/pages/warehouse/record/index.tsx
+++ website/pages/warehouse/record/index.tsx
@@ -1,6 +1,5 @@
 // 借还记录
-import { defineComponent, onMounted, ref } from "vue";
-import { Tabs } from "website/components";
+import { defineComponent, ref } from "vue";
 
 import EventList from "./eventList";
 import DailyList from "./dailyList";
@@ -13,59 +12,23 @@ export default defineComponent({
     MaintainList,
   },
   setup() {
-    // 菜单配置项
-    const menus = ref([
-      {
-        label: "事件记录",
-        key: "event",
-      },
-      {
-        label: "日常记录",
-        key: "daily",
-      },
-      {
-        label: "维修记录",
-        key: "1",
-      },
-      {
-        label: "保养记录",
-        key: "2",
-      },
-    ]);
-    const componentsName = ref("");
-    // 菜单列表空状态
-    const menuEmpty = ref(false);
-    // 菜单当前激活值
-    const menuActiveKey = ref(menus.value[0].key); //监听点击标签页菜单事件
-    // 监听点击标签页菜单事件
-    const handleClickTabPane = (activeKey = menuActiveKey.value) => {
-      if (activeKey === "1" || activeKey === "2") {
-        componentsName.value = "MaintainList";
-      } else if (activeKey === "event") {
-        componentsName.value = "EventList";
-      } else if (activeKey === "daily") {
-        componentsName.value = "DailyList";
-      }
-    };
-    onMounted(() => {
-      handleClickTabPane();
-    });
-
+    const menuActiveKey = ref("event");
     return () => (
       <>
-        <Tabs
-          class="dark:bg-navy-4"
-          v-model={[menuActiveKey.value, "activeKey"]}
-          columns={menus.value}
-          empty={menuEmpty.value}
-          onClick={handleClickTabPane}
-          rowKey={(index) => index}
-          size="small"
-        >
-          <section class="overflow-y-auto">
-            <component is={componentsName.value}></component>
-          </section>
-        </Tabs>
+        <a-tabs v-model={menuActiveKey.value} class="dark:bg-navy-4">
+          <a-tab-pane key={"event"} tab="事件记录">
+            <EventList></EventList>
+          </a-tab-pane>
+          <a-tab-pane key={"daily"} tab="日常记录">
+            <DailyList></DailyList>
+          </a-tab-pane>
+          <a-tab-pane key={"1"} tab="维修记录">
+            <MaintainList menuActiveKey={"1"}></MaintainList>
+          </a-tab-pane>
+          <a-tab-pane key={"2"} tab="保养记录">
+            <MaintainList menuActiveKey={"2"}></MaintainList>
+          </a-tab-pane>
+        </a-tabs>
       </>
     );
   },
