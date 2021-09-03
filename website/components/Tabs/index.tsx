c67cd47b2cb3837940a65af944b4acb977a5f1ff
Damien Lau <damienlaus@icloud.com>
2021-09-03 14:27:24 +0800

    fix: change dir.

--- website/components/Tabs/index.tsx
+++ website/components/Tabs/index.tsx
@@ -1,23 +1,20 @@
-import { computed, defineComponent, onMounted, ref, toRefs } from "vue";
+import { computed, defineComponent, onMounted, ref } from "vue";
 import { Badge, TabPane, Tabs } from "ant-design-vue";
 
-type TabPaneProps = {
-  label: string;
-  key: string;
+export interface TabPaneProps {
+  label?: string;
+  key?: string;
   count?: number;
-};
+  [propertyName: string]: any;
+}
 
 export default defineComponent({
   name: "Tabs",
-  emits: {
-    click: (payload: string | number) => {
-      return true;
-    },
-  },
   props: {
     center: { type: Boolean, required: false, default: false },
     columns: { type: Object, required: true },
   },
+  emits: ["click"],
   setup(props, { slots, emit }) {
     const tabActiveKey = ref<string | number>(props.columns[0].key);
     const tabNavbarCenterClasses = computed(() => {
@@ -27,7 +24,13 @@ export default defineComponent({
     });
 
     const handleClick = (activeKey = tabActiveKey.value) => {
-      emit("click", activeKey);
+      let selectedTabPane: TabPaneProps = props.columns.find(
+        (row: TabPaneProps) => {
+          return row.key === activeKey;
+        }
+      );
+
+      emit("click", { activeKey, item: selectedTabPane });
     };
 
     onMounted(() => {
@@ -39,7 +42,7 @@ export default defineComponent({
         class={tabNavbarCenterClasses.value}
         defaultActiveKey={tabActiveKey.value}
         animated={false}
-        onTabClick={handleClick}
+        onChange={handleClick}
       >
         {{
           default: () => {
