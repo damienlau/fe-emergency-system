986ffc1defaa19790cd3c37de55a603dc7c2b542
Damien Lau <damienlaus@icloud.com>
2021-09-08 10:17:59 +0800

    test: some change.

--- website/pages/warehouse/meterial/shelf/index.vue
+++ website/pages/warehouse/meterial/shelf/index.vue
@@ -1,7 +1,38 @@
-<template></template>
+<template>
+  <section class="w-full h-full">
+    <Tabs :columns="tabsConfig">
+      <Form :columns="formConfig"></Form>
+    </Tabs>
+  </section>
+</template>
 
 <script>
+import Tabs from "components/Tabs";
+import Form from "components/Form";
+import { ref } from "vue";
 export default {
-  setup() {},
+  components: { Form, Tabs },
+  setup() {
+    const formData = ref(null);
+    const tabsConfig = ref([
+      { label: "第一", key: 1 },
+      { label: "第二", key: 2 },
+    ]);
+    const formConfig = ref([
+      { label: "第一", key: 1 },
+      { label: "第二", key: 2 },
+      {
+        label: "第三",
+        key: 3,
+        type: "select",
+        options: [
+          { label: "第一", value: 1 },
+          { label: "第二", value: 2 },
+        ],
+      },
+    ]);
+
+    return { formData, formConfig, tabsConfig };
+  },
 };
 </script>
