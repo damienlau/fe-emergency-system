034ab59050a3b7be86b2585e60c578f1a2175971
libo <1186279432@qq.com>
2021-09-10 09:07:17 +0800

    fix:接口调试
    接口调试

--- website/store/modules/warehouse/pending.js
+++ website/store/modules/warehouse/pending.js
@@ -1,6 +1,13 @@
 import {
   findSpecifiedShortcutData,
-  saveSpecifiedShortcutData
+  saveSpecifiedShortcutData,
+  findDetailSpecifiedShortcutData,
+  returnSpecifiedShortcutData,
+  sweepGateOpen,
+  sweepGateReader,
+  sweepGateClose,
+  allBoxinfoPending,
+  allmaterialPending
 } from "api/warehouse/pending";
 
 const state = () => ({});
@@ -10,16 +17,79 @@ const actions = {
   findSpecifiedShortcutList: ({ dispatch },params) => {
     return new Promise((reslove) => {
       findSpecifiedShortcutData(params).then((res) => {
-        reslove(res.data);
+        reslove(res);
+      });
+    });
+  },
+
+  // 待出仓物资数据明细
+  findDetailSpecifiedShortcutList: ({ dispatch },params) => {
+    return new Promise((reslove) => {
+      findDetailSpecifiedShortcutData(params).then((res) => {
+        reslove(res);
       });
     });
   },
   //待出仓物资确认
-
   saveSpecifiedShortcutSure: ({ dispatch }, params) => {
     return new Promise((reslove) => {
       saveSpecifiedShortcutData(params).then((res) => {
-        reslove(res.data);
+        reslove(res);
+      })
+    })
+  },
+
+  
+  //归仓物资确认
+  returnSpecifiedShortcutList: ({ dispatch }, params) => {
+    return new Promise((reslove) => {
+      returnSpecifiedShortcutData(params).then((res) => {
+        reslove(res);
+      })
+    })
+  },
+
+  //扫描门开启
+  sweepGateOpenData: ({ dispatch }, params) => {
+    return new Promise((reslove) => {
+      sweepGateOpen(params).then((res) => {
+        reslove(res);
+      })
+    })
+  },
+  
+  //扫描门读取
+  sweepGateReaderData: ({ dispatch }, params) => {
+    return new Promise((reslove) => {
+      sweepGateReader(params).then((res) => {
+        reslove(res);
+      })
+    })
+  },
+
+  //扫描门关闭
+  sweepGateCloseData: ({ dispatch }, params) => {
+    return new Promise((reslove) => {
+      sweepGateClose(params).then((res) => {
+        reslove(res);
+      })
+    })
+  },
+
+  //全部仓库箱子
+  allBoxinfoPendingData: ({ dispatch }, params) => {
+    return new Promise((reslove) => {
+      allBoxinfoPending(params).then((res) => {
+        reslove(res);
+      })
+    })
+  },
+
+  //全部仓库物资
+  allmaterialPendingData: ({ dispatch }, params) => {
+    return new Promise((reslove) => {
+      allmaterialPending(params).then((res) => {
+        reslove(res);
       })
     })
   }
