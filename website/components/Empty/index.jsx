6eb31d52bd4d866142722509ac1b960a2aa14de4
libo <1186279432@qq.com>
2021-09-18 19:50:51 +0800

    fix：修改

--- website/components/Empty/index.jsx
+++
@@ -1,9 +0,0 @@
-// 组件-空状态
-
-import { defineComponent } from "vue";
-
-export default defineComponent({
-  setup() {
-    return () => <a-empty image={`assets/icon_empty_data.png`}></a-empty>;
-  },
-});
