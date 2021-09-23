d93c24f12a49470143d1e06888d720cc624ea7fa
Garen <damienlaus@icloud.com>
2021-09-23 15:48:25 +0800

    refactor: sidebar.

--- website/pages/warehouse/material/sidebar.tsx
+++ website/pages/warehouse/material/sidebar.tsx
@@ -37,6 +37,23 @@ export default defineComponent({
         alias: "新增箱子",
       },
     ]);
+    const selectOptions = ref([
+      { key: 1, label: "急救/重症" },
+      { key: 2, label: "门诊" },
+      { key: 3, label: "后勤" },
+      { key: 4, label: "指挥" },
+      { key: 5, label: "重症" },
+      { key: 6, label: "超声" },
+      { key: 7, label: "清创" },
+      { key: 8, label: "留观" },
+      { key: 9, label: "药房" },
+      { key: 10, label: "耗材" },
+      { key: 11, label: "手术" },
+      { key: 12, label: "防疫/隔离" },
+      { key: 13, label: "消毒" },
+      { key: 14, label: "住院" },
+      { key: 15, label: "检验" },
+    ]);
     const selectedTabPane = ref<tabPaneProps>({});
     const searchForm = ref({});
     const cardData = ref<goodsRequestProps[] | boxRequestProps[]>([]);
@@ -101,6 +118,7 @@ export default defineComponent({
                     <Form
                       model={searchForm.value}
                       layout={!collapsed.value ? "inline" : "horizontal"}
+                      wrapperCol={{ span: 24 }}
                     >
                       <FormItem>
                         {/* Search Input */}
@@ -113,11 +131,16 @@ export default defineComponent({
                         <FormItem label="类型">
                           {/* Select */}
                           <Select>
-                            {
-                              <SelectOption key="" title="">
-                                {}
-                              </SelectOption>
-                            }
+                            {selectOptions.value.map((option) => {
+                              return (
+                                <SelectOption
+                                  key={option.key}
+                                  title={option.label}
+                                >
+                                  {option.label}
+                                </SelectOption>
+                              );
+                            })}
                           </Select>
                         </FormItem>
                       )}
@@ -140,12 +163,6 @@ export default defineComponent({
                         {/* Body */}
                         <div class="flex flex-row border-b border-navy-1 pb-8">
                           {/* thumbnail */}
-                          {/* <Image
-                            width={162}
-                            height={162}
-                            src={lists.materialImages}
-                            fallback={emptyImage}
-                          ></Image> */}
                           <Images
                             columns={lists.materialImages}
                             size={162}
